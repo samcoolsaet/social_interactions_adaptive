1aeb7b35708239c9b905015e41f96f4d9338a370
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-06-10 10:04:14 -0400

    update

--- UpdateInventory.m
+++ UpdateInventory.m
@@ -20,7 +20,13 @@ end
 
 % pix_per_deg =  MLConfig.PixelsPerDegree.
 % Images are 512*288
-% I tilt the images 90 degrees to the left, so my start pixel ( upper left corner ) point becomes [0 512], so y pixels become x and x pixels become (512-y)
+% I tilt the images 90 degrees to the left, so my start pixel ( upper left corner ) point becomes [0 512], so x pixels become y and y pixels become (512-x)
+test = zeros(1, length(inventory),2);
+for i = 1:length(inventory)
+    if ~isempty(inventory(i).a_origin)
+        test(1,i,1:2) = [inventory(i).a_origin(2) (512-inventory(i).a_origin(1))];
+    end
+end
 % I'm scaling images up with 1.25, which makes the size 640*360 during the task
 % Keeping both of this into account, the center ( where degrees start 0 ) is [180 320] in pixels
 % to centralize the source to the middle of the screen in pixels, I would have have to substract the center [ 320 180 ]from the original pixel coordinates.
