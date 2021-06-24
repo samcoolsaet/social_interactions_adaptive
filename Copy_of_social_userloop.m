d1b1eea5135947e69211cbc0ad1096dbb66ccb50
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-06-24 11:53:11 -0400

    update

--- Copy_of_social_userloop.m
+++ Copy_of_social_userloop.m
@@ -172,114 +172,126 @@ TrialRecord.User.current_sum_buttons = sum([TrialRecord.User.chasing_on, ... % d
 % Do all of this for trial 0 because it would be inefficient to repeat and
 % I want these variables to be fixed throughout the run
 if TrialRecord.CurrentTrialNumber == 0
-    % dir() gives a struct of the contents of the path
-    chasing_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\chasing');
-    grooming_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\grooming');
-    mounting_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\mounting');
-    holding_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\holding');
-    
-    chasing_path = regexp(chasing_struct(1).folder,filesep,'split');
-    grooming_path = regexp(grooming_struct(1).folder,filesep,'split');
-    mounting_path = regexp(mounting_struct(1).folder,filesep,'split');
-    holding_path = regexp(holding_struct(1).folder,filesep,'split');
-    
-    TrialRecord.User.chasing_folder = chasing_path{1, end};
-    TrialRecord.User.grooming_folder = grooming_path{1, end};
-    TrialRecord.User.mounting_folder = mounting_path{1, end};
-    TrialRecord.User.holding_folder = holding_path{1, end};
-    
-    % isolating the name field
-    TrialRecord.User.chasing_list = {chasing_struct.name};
-    TrialRecord.User.grooming_list = {grooming_struct.name};
-    TrialRecord.User.mounting_list = {mounting_struct.name};
-    TrialRecord.User.holding_list = {holding_struct.name};
-    
-    % deleting the 2 empty spots from the name field
-    TrialRecord.User.chasing_list(1:2) = [];
-    TrialRecord.User.grooming_list(1:2) = [];
-    TrialRecord.User.mounting_list(1:2) = [];
-    TrialRecord.User.holding_list(1:2) = [];
-    
-
-    % analogous for the frames
-    chasing_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\chasing');
-    grooming_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\grooming');
-    mounting_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\mounting');
-    holding_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\holding');
-
-    TrialRecord.User.chasing_frame_list = {chasing_frame_struct.name};
-    TrialRecord.User.grooming_frame_list = {grooming_frame_struct.name};
-    TrialRecord.User.mounting_frame_list = {mounting_frame_struct.name};
-    TrialRecord.User.holding_frame_list = {holding_frame_struct.name};
-
-    TrialRecord.User.chasing_frame_list(1:2) = [];
-    TrialRecord.User.grooming_frame_list(1:2) = [];
-    TrialRecord.User.mounting_frame_list(1:2) = [];
-    TrialRecord.User.holding_frame_list(1:2) = [];
-
-if TrialRecord.User.generalizing
-    gen_chasing_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_chasing');
-    gen_grooming_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_grooming');
-    gen_mounting_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_mounting');
-    gen_holding_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_holding');
-    
-    gen_chasing_path = regexp(gen_chasing_struct(1).folder,filesep,'split');
-    gen_grooming_path = regexp(gen_grooming_struct(1).folder,filesep,'split');
-    gen_mounting_path = regexp(gen_mounting_struct(1).folder,filesep,'split');
-    gen_holding_path = regexp(gen_holding_struct(1).folder,filesep,'split');
-    
-    TrialRecord.User.gen_chasing_folder = gen_chasing_path{1, end};
-    TrialRecord.User.gen_grooming_folder = gen_grooming_path{1, end};
-    TrialRecord.User.gen_mounting_folder = gen_mounting_path{1, end};
-    TrialRecord.User.gen_holding_folder = gen_holding_path{1, end};
-
-    TrialRecord.User.gen_chasing_list = {gen_chasing_struct.name};
-    TrialRecord.User.gen_grooming_list = {gen_grooming_struct.name};
-    TrialRecord.User.gen_mounting_list = {gen_mounting_struct.name};
-    TrialRecord.User.gen_holding_list = {gen_holding_struct.name};
-    
-    TrialRecord.User.gen_chasing_list(1:2) = [];
-    TrialRecord.User.gen_grooming_list(1:2) = [];
-    TrialRecord.User.gen_mounting_list(1:2) = [];
-    TrialRecord.User.gen_holding_list(1:2) = [];
-    
-    gen_chasing_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_chasing');
-    gen_grooming_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_grooming');
-    gen_mounting_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_mounting');
-    gen_holding_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_holding');
-    
-    TrialRecord.User.gen_chasing_frame_list = {gen_chasing_frame_struct.name};
-    TrialRecord.User.gen_grooming_frame_list = {gen_grooming_frame_struct.name};
-    TrialRecord.User.gen_mounting_frame_list = {gen_mounting_frame_struct.name};
-    TrialRecord.User.gen_holding_frame_list = {gen_holding_frame_struct.name};
-
-    TrialRecord.User.gen_chasing_frame_list(1:2) = [];
-    TrialRecord.User.gen_grooming_frame_list(1:2) = [];
-    TrialRecord.User.gen_mounting_frame_list(1:2) = [];
-    TrialRecord.User.gen_holding_frame_list(1:2) = [];
-else
-    TrialRecord.User.gen_chasing_list = [];
-    TrialRecord.User.gen_grooming_list = [];
-    TrialRecord.User.gen_mounting_list = [];
-    TrialRecord.User.gen_holding_list = [];
-    
-    TrialRecord.User.gen_chasing_frame_list = [];
-    TrialRecord.User.gen_grooming_frame_list = [];
-    TrialRecord.User.gen_mounting_frame_list = [];
-    TrialRecord.User.gen_holding_frame_list = [];
+    [TrialRecord.User.chasing_list, TrialRecord.User.grooming_list, TrialRecord.User.mounting_list, ...
+        TrialRecord.User.holding_list, TrialRecord.User.gen_chasing_list, TrialRecord.User.gen_grooming_list, ...
+        TrialRecord.User.gen_mounting_list, TrialRecord.User.gen_holding_list,...
+    TrialRecord.User.chasing_frame_list, TrialRecord.User.grooming_frame_list, TrialRecord.User.mounting_frame_list, ...
+    TrialRecord.User.holding_frame_list, TrialRecord.User.gen_chasing_frame_list, ...
+    TrialRecord.User.gen_grooming_frame_list, TrialRecord.User.gen_mounting_frame_list, ...
+    TrialRecord.User.gen_holding_frame_list, TrialRecord.User.chasing_folder, TrialRecord.User.grooming_folder, ...
+    TrialRecord.User.mounting_folder, TrialRecord.User.holding_folder, TrialRecord.User.gen_chasing_folder, ...
+    TrialRecord.User.gen_grooming_folder, TrialRecord.User.gen_mounting_folder, TrialRecord.User.gen_holding_folder,...
+    TrialRecord.User.general_stimulus_list, TrialRecord.User.general_frame_list] = ...
+    stimulusList('D:\onedrive\OneDrive - KU Leuven\social_interactions', TrialRecord.User.generalizing);
 end
-
-    % creating general lists with all the files
-    TrialRecord.User.general_stimulus_list = [TrialRecord.User.chasing_list, ...
-        TrialRecord.User.gen_chasing_list, TrialRecord.User.grooming_list,...
-        TrialRecord.User.gen_grooming_list, TrialRecord.User.mounting_list,...
-        TrialRecord.User.gen_mounting_list, TrialRecord.User.holding_list, TrialRecord.User.gen_holding_list]; 
-    TrialRecord.User.general_frame_list = [TrialRecord.User.chasing_frame_list,...
-        TrialRecord.User.gen_chasing_frame_list, TrialRecord.User.grooming_frame_list,...
-        TrialRecord.User.gen_grooming_frame_list,TrialRecord.User.mounting_frame_list,...
-        TrialRecord.User.gen_mounting_frame_list, TrialRecord.User.holding_frame_list,...
-        TrialRecord.User.gen_holding_frame_list];
-end
+%     % dir() gives a struct of the contents of the path
+%     chasing_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\chasing');
+%     grooming_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\grooming');
+%     mounting_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\mounting');
+%     holding_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\holding');
+%     
+%     chasing_path = regexp(chasing_struct(1).folder,filesep,'split');
+%     grooming_path = regexp(grooming_struct(1).folder,filesep,'split');
+%     mounting_path = regexp(mounting_struct(1).folder,filesep,'split');
+%     holding_path = regexp(holding_struct(1).folder,filesep,'split');
+%     
+%     TrialRecord.User.chasing_folder = chasing_path{1, end};
+%     TrialRecord.User.grooming_folder = grooming_path{1, end};
+%     TrialRecord.User.mounting_folder = mounting_path{1, end};
+%     TrialRecord.User.holding_folder = holding_path{1, end};
+%     
+%     % isolating the name field
+%     TrialRecord.User.chasing_list = {chasing_struct.name};
+%     TrialRecord.User.grooming_list = {grooming_struct.name};
+%     TrialRecord.User.mounting_list = {mounting_struct.name};
+%     TrialRecord.User.holding_list = {holding_struct.name};
+%     
+%     % deleting the 2 empty spots from the name field
+%     TrialRecord.User.chasing_list(1:2) = [];
+%     TrialRecord.User.grooming_list(1:2) = [];
+%     TrialRecord.User.mounting_list(1:2) = [];
+%     TrialRecord.User.holding_list(1:2) = [];
+%     
+% 
+%     % analogous for the frames
+%     chasing_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\chasing');
+%     grooming_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\grooming');
+%     mounting_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\mounting');
+%     holding_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\holding');
+% 
+%     TrialRecord.User.chasing_frame_list = {chasing_frame_struct.name};
+%     TrialRecord.User.grooming_frame_list = {grooming_frame_struct.name};
+%     TrialRecord.User.mounting_frame_list = {mounting_frame_struct.name};
+%     TrialRecord.User.holding_frame_list = {holding_frame_struct.name};
+% 
+%     TrialRecord.User.chasing_frame_list(1:2) = [];
+%     TrialRecord.User.grooming_frame_list(1:2) = [];
+%     TrialRecord.User.mounting_frame_list(1:2) = [];
+%     TrialRecord.User.holding_frame_list(1:2) = [];
+% 
+% if TrialRecord.User.generalizing
+%     gen_chasing_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_chasing');
+%     gen_grooming_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_grooming');
+%     gen_mounting_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_mounting');
+%     gen_holding_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_holding');
+%     
+%     gen_chasing_path = regexp(gen_chasing_struct(1).folder,filesep,'split');
+%     gen_grooming_path = regexp(gen_grooming_struct(1).folder,filesep,'split');
+%     gen_mounting_path = regexp(gen_mounting_struct(1).folder,filesep,'split');
+%     gen_holding_path = regexp(gen_holding_struct(1).folder,filesep,'split');
+%     
+%     TrialRecord.User.gen_chasing_folder = gen_chasing_path{1, end};
+%     TrialRecord.User.gen_grooming_folder = gen_grooming_path{1, end};
+%     TrialRecord.User.gen_mounting_folder = gen_mounting_path{1, end};
+%     TrialRecord.User.gen_holding_folder = gen_holding_path{1, end};
+% 
+%     TrialRecord.User.gen_chasing_list = {gen_chasing_struct.name};
+%     TrialRecord.User.gen_grooming_list = {gen_grooming_struct.name};
+%     TrialRecord.User.gen_mounting_list = {gen_mounting_struct.name};
+%     TrialRecord.User.gen_holding_list = {gen_holding_struct.name};
+%     
+%     TrialRecord.User.gen_chasing_list(1:2) = [];
+%     TrialRecord.User.gen_grooming_list(1:2) = [];
+%     TrialRecord.User.gen_mounting_list(1:2) = [];
+%     TrialRecord.User.gen_holding_list(1:2) = [];
+%     
+%     gen_chasing_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_chasing');
+%     gen_grooming_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_grooming');
+%     gen_mounting_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_mounting');
+%     gen_holding_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_holding');
+%     
+%     TrialRecord.User.gen_chasing_frame_list = {gen_chasing_frame_struct.name};
+%     TrialRecord.User.gen_grooming_frame_list = {gen_grooming_frame_struct.name};
+%     TrialRecord.User.gen_mounting_frame_list = {gen_mounting_frame_struct.name};
+%     TrialRecord.User.gen_holding_frame_list = {gen_holding_frame_struct.name};
+% 
+%     TrialRecord.User.gen_chasing_frame_list(1:2) = [];
+%     TrialRecord.User.gen_grooming_frame_list(1:2) = [];
+%     TrialRecord.User.gen_mounting_frame_list(1:2) = [];
+%     TrialRecord.User.gen_holding_frame_list(1:2) = [];
+% else
+%     TrialRecord.User.gen_chasing_list = [];
+%     TrialRecord.User.gen_grooming_list = [];
+%     TrialRecord.User.gen_mounting_list = [];
+%     TrialRecord.User.gen_holding_list = [];
+%     
+%     TrialRecord.User.gen_chasing_frame_list = [];
+%     TrialRecord.User.gen_grooming_frame_list = [];
+%     TrialRecord.User.gen_mounting_frame_list = [];
+%     TrialRecord.User.gen_holding_frame_list = [];
+% end
+% 
+%     % creating general lists with all the files
+%     TrialRecord.User.general_stimulus_list = [TrialRecord.User.chasing_list, ...
+%         TrialRecord.User.gen_chasing_list, TrialRecord.User.grooming_list,...
+%         TrialRecord.User.gen_grooming_list, TrialRecord.User.mounting_list,...
+%         TrialRecord.User.gen_mounting_list, TrialRecord.User.holding_list, TrialRecord.User.gen_holding_list]; 
+%     TrialRecord.User.general_frame_list = [TrialRecord.User.chasing_frame_list,...
+%         TrialRecord.User.gen_chasing_frame_list, TrialRecord.User.grooming_frame_list,...
+%         TrialRecord.User.gen_grooming_frame_list,TrialRecord.User.mounting_frame_list,...
+%         TrialRecord.User.gen_mounting_frame_list, TrialRecord.User.holding_frame_list,...
+%         TrialRecord.User.gen_holding_frame_list];
+% end
 
 cum_length = cumsum([length(TrialRecord.User.chasing_list) length(TrialRecord.User.gen_chasing_list)...
     length(TrialRecord.User.grooming_list) length(TrialRecord.User.gen_grooming_list)...
--- stimulusList.m
+++ stimulusList.m
@@ -4,114 +4,114 @@ function [chasing_list, grooming_list, mounting_list, holding_list,...
     gen_chasing_frame_list, gen_grooming_frame_list, gen_mounting_frame_list, gen_holding_frame_list,...
     chasing_folder, grooming_folder, mounting_folder, holding_folder,...
     gen_chasing_folder, gen_grooming_folder, gen_mounting_folder, gen_holding_folder,...
-    general_stimulus_list, general_frame_list] = stimulusList(inputArg1,inputArg2)
+    general_stimulus_list, general_frame_list] = stimulusList(root, generalizing) % 'D:\onedrive\OneDrive - KU Leuven\social_interactions'
 
-end
     % dir() gives a struct of the contents of the path
-    chasing_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\chasing');
-    grooming_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\grooming');
-    mounting_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\mounting');
-    holding_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\holding');
+    chasing_struct = dir(strcat(root,'\stimuli\chasing'));
+    grooming_struct = dir(strcat(root,'\stimuli\grooming'));
+    mounting_struct = dir(strcat(root,'\stimuli\mounting'));
+    holding_struct = dir(strcat(root,'\stimuli\holding'));
     
     chasing_path = regexp(chasing_struct(1).folder,filesep,'split');
     grooming_path = regexp(grooming_struct(1).folder,filesep,'split');
     mounting_path = regexp(mounting_struct(1).folder,filesep,'split');
     holding_path = regexp(holding_struct(1).folder,filesep,'split');
     
-    TrialRecord.User.chasing_folder = chasing_path{1, end};
-    TrialRecord.User.grooming_folder = grooming_path{1, end};
-    TrialRecord.User.mounting_folder = mounting_path{1, end};
-    TrialRecord.User.holding_folder = holding_path{1, end};
+    chasing_folder = chasing_path{1, end};
+    grooming_folder = grooming_path{1, end};
+    mounting_folder = mounting_path{1, end};
+    holding_folder = holding_path{1, end};
     
     % isolating the name field
-    TrialRecord.User.chasing_list = {chasing_struct.name};
-    TrialRecord.User.grooming_list = {grooming_struct.name};
-    TrialRecord.User.mounting_list = {mounting_struct.name};
-    TrialRecord.User.holding_list = {holding_struct.name};
+    chasing_list = {chasing_struct.name};
+    grooming_list = {grooming_struct.name};
+    mounting_list = {mounting_struct.name};
+    holding_list = {holding_struct.name};
     
     % deleting the 2 empty spots from the name field
-    TrialRecord.User.chasing_list(1:2) = [];
-    TrialRecord.User.grooming_list(1:2) = [];
-    TrialRecord.User.mounting_list(1:2) = [];
-    TrialRecord.User.holding_list(1:2) = [];
+    chasing_list(1:2) = [];
+    grooming_list(1:2) = [];
+    mounting_list(1:2) = [];
+    holding_list(1:2) = [];
     
 
     % analogous for the frames
-    chasing_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\chasing');
-    grooming_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\grooming');
-    mounting_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\mounting');
-    holding_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\holding');
+    chasing_frame_struct = dir(strcat(root,'\frames\chasing'));
+    grooming_frame_struct = dir(strcat(root,'\frames\grooming'));
+    mounting_frame_struct = dir(strcat(root,'\frames\mounting'));
+    holding_frame_struct = dir(strcat(root,'\frames\holding'));
 
-    TrialRecord.User.chasing_frame_list = {chasing_frame_struct.name};
-    TrialRecord.User.grooming_frame_list = {grooming_frame_struct.name};
-    TrialRecord.User.mounting_frame_list = {mounting_frame_struct.name};
-    TrialRecord.User.holding_frame_list = {holding_frame_struct.name};
+    chasing_frame_list = {chasing_frame_struct.name};
+    grooming_frame_list = {grooming_frame_struct.name};
+    mounting_frame_list = {mounting_frame_struct.name};
+    holding_frame_list = {holding_frame_struct.name};
 
-    TrialRecord.User.chasing_frame_list(1:2) = [];
-    TrialRecord.User.grooming_frame_list(1:2) = [];
-    TrialRecord.User.mounting_frame_list(1:2) = [];
-    TrialRecord.User.holding_frame_list(1:2) = [];
+    chasing_frame_list(1:2) = [];
+    grooming_frame_list(1:2) = [];
+    mounting_frame_list(1:2) = [];
+    holding_frame_list(1:2) = [];
 
-if TrialRecord.User.generalizing
-    gen_chasing_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_chasing');
-    gen_grooming_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_grooming');
-    gen_mounting_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_mounting');
-    gen_holding_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\stimuli\gen_holding');
+if generalizing
+    gen_chasing_struct = dir(strcat(root,'\stimuli\gen_chasing'));
+    gen_grooming_struct = dir(strcat(root,'\stimuli\gen_grooming'));
+    gen_mounting_struct = dir(strcat(root,'\stimuli\gen_mounting'));
+    gen_holding_struct = dir(strcat(root,'\stimuli\gen_holding'));
     
     gen_chasing_path = regexp(gen_chasing_struct(1).folder,filesep,'split');
     gen_grooming_path = regexp(gen_grooming_struct(1).folder,filesep,'split');
     gen_mounting_path = regexp(gen_mounting_struct(1).folder,filesep,'split');
     gen_holding_path = regexp(gen_holding_struct(1).folder,filesep,'split');
     
-    TrialRecord.User.gen_chasing_folder = gen_chasing_path{1, end};
-    TrialRecord.User.gen_grooming_folder = gen_grooming_path{1, end};
-    TrialRecord.User.gen_mounting_folder = gen_mounting_path{1, end};
-    TrialRecord.User.gen_holding_folder = gen_holding_path{1, end};
+    gen_chasing_folder = gen_chasing_path{1, end};
+    gen_grooming_folder = gen_grooming_path{1, end};
+    gen_mounting_folder = gen_mounting_path{1, end};
+    gen_holding_folder = gen_holding_path{1, end};
 
-    TrialRecord.User.gen_chasing_list = {gen_chasing_struct.name};
-    TrialRecord.User.gen_grooming_list = {gen_grooming_struct.name};
-    TrialRecord.User.gen_mounting_list = {gen_mounting_struct.name};
-    TrialRecord.User.gen_holding_list = {gen_holding_struct.name};
+    gen_chasing_list = {gen_chasing_struct.name};
+    gen_grooming_list = {gen_grooming_struct.name};
+    gen_mounting_list = {gen_mounting_struct.name};
+    gen_holding_list = {gen_holding_struct.name};
     
-    TrialRecord.User.gen_chasing_list(1:2) = [];
-    TrialRecord.User.gen_grooming_list(1:2) = [];
-    TrialRecord.User.gen_mounting_list(1:2) = [];
-    TrialRecord.User.gen_holding_list(1:2) = [];
+    gen_chasing_list(1:2) = [];
+    gen_grooming_list(1:2) = [];
+    gen_mounting_list(1:2) = [];
+    gen_holding_list(1:2) = [];
     
-    gen_chasing_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_chasing');
-    gen_grooming_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_grooming');
-    gen_mounting_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_mounting');
-    gen_holding_frame_struct = dir('D:\onedrive\OneDrive - KU Leuven\social_interactions\frames\gen_holding');
+    gen_chasing_frame_struct = dir(strcat(root,'\frames\gen_chasing'));
+    gen_grooming_frame_struct = dir(strcat(root,'\frames\gen_grooming'));
+    gen_mounting_frame_struct = dir(strcat(root,'\frames\gen_mounting'));
+    gen_holding_frame_struct = dir(strcat(root,'\frames\gen_holding'));
     
-    TrialRecord.User.gen_chasing_frame_list = {gen_chasing_frame_struct.name};
-    TrialRecord.User.gen_grooming_frame_list = {gen_grooming_frame_struct.name};
-    TrialRecord.User.gen_mounting_frame_list = {gen_mounting_frame_struct.name};
-    TrialRecord.User.gen_holding_frame_list = {gen_holding_frame_struct.name};
+    gen_chasing_frame_list = {gen_chasing_frame_struct.name};
+    gen_grooming_frame_list = {gen_grooming_frame_struct.name};
+    gen_mounting_frame_list = {gen_mounting_frame_struct.name};
+    gen_holding_frame_list = {gen_holding_frame_struct.name};
 
-    TrialRecord.User.gen_chasing_frame_list(1:2) = [];
-    TrialRecord.User.gen_grooming_frame_list(1:2) = [];
-    TrialRecord.User.gen_mounting_frame_list(1:2) = [];
-    TrialRecord.User.gen_holding_frame_list(1:2) = [];
+    gen_chasing_frame_list(1:2) = [];
+    gen_grooming_frame_list(1:2) = [];
+    gen_mounting_frame_list(1:2) = [];
+    gen_holding_frame_list(1:2) = [];
 else
-    TrialRecord.User.gen_chasing_list = [];
-    TrialRecord.User.gen_grooming_list = [];
-    TrialRecord.User.gen_mounting_list = [];
-    TrialRecord.User.gen_holding_list = [];
+    gen_chasing_list = [];
+    gen_grooming_list = [];
+    gen_mounting_list = [];
+    gen_holding_list = [];
     
-    TrialRecord.User.gen_chasing_frame_list = [];
-    TrialRecord.User.gen_grooming_frame_list = [];
-    TrialRecord.User.gen_mounting_frame_list = [];
-    TrialRecord.User.gen_holding_frame_list = [];
+    gen_chasing_frame_list = [];
+    gen_grooming_frame_list = [];
+    gen_mounting_frame_list = [];
+    gen_holding_frame_list = [];
 end
 
     % creating general lists with all the files
-    TrialRecord.User.general_stimulus_list = [TrialRecord.User.chasing_list, ...
-        TrialRecord.User.gen_chasing_list, TrialRecord.User.grooming_list,...
-        TrialRecord.User.gen_grooming_list, TrialRecord.User.mounting_list,...
-        TrialRecord.User.gen_mounting_list, TrialRecord.User.holding_list, TrialRecord.User.gen_holding_list]; 
-    TrialRecord.User.general_frame_list = [TrialRecord.User.chasing_frame_list,...
-        TrialRecord.User.gen_chasing_frame_list, TrialRecord.User.grooming_frame_list,...
-        TrialRecord.User.gen_grooming_frame_list,TrialRecord.User.mounting_frame_list,...
-        TrialRecord.User.gen_mounting_frame_list, TrialRecord.User.holding_frame_list,...
-        TrialRecord.User.gen_holding_frame_list];
+    general_stimulus_list = [chasing_list, ...
+        gen_chasing_list, grooming_list,...
+        gen_grooming_list, mounting_list,...
+        gen_mounting_list, holding_list, gen_holding_list]; 
+    general_frame_list = [chasing_frame_list,...
+        gen_chasing_frame_list, grooming_frame_list,...
+        gen_grooming_frame_list,mounting_frame_list,...
+        gen_mounting_frame_list, holding_frame_list,...
+        gen_holding_frame_list];
+end
 
