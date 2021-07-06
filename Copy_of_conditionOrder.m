fa9bd02c261960bbed7b09c2af51a5173d8bfa43
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-07-06 14:18:52 -0400

    update

---
+++ Copy_of_conditionOrder.m
@@ -0,0 +1,82 @@
+function [conditions_array, rnd_condition_order] = Copy_of_conditionOrder(structure, max_repeats, rnd_condition_order,...
+    categorizing, agent_patient)
+
+struct_conditions = zeros(length(structure), 3);
+for i = 1:length(structure)
+    struct_conditions(i, :) = structure(i).condition;
+end
+
+if categorizing
+    active_conditions = struct_conditions(:,1);
+    not_completed_indexes = [structure.c_completed]==0;
+    free_conditions = active_conditions(not_completed_indexes)';
+elseif agent_patient
+    a_active_conditions = struct_conditions(:,2);
+    p_active_conditions = struct_conditions(:,3);
+    not_completed_indexes = [[structure.a_completed]==0; [structure.p_completed]==0];
+    a_available = a_active_conditions(not_completed_indexes(1,:));
+    p_available = p_active_conditions(not_completed_indexes(2,:));
+    free_conditions = [a_available' p_available'];
+elseif ~agent_patient && ~categorizing    
+    c_active_conditions = struct_conditions(:,1);
+    a_active_conditions = struct_conditions(:,2);
+    p_active_conditions = struct_conditions(:,3);
+    not_completed_indexes = [[structure.c_completed]==0; [structure.a_completed]==0; [structure.p_completed]==0];
+    c_available = c_active_conditions(not_completed_indexes(1,:));
+    a_available = a_active_conditions(not_completed_indexes(2,:));
+    p_available = p_active_conditions(not_completed_indexes(3,:));
+    free_conditions = [c_available' a_available' p_available'];
+end
+
+starting_no_conditions  = histc(free_conditions, unique(free_conditions));
+
+conditions_array = [unique(free_conditions); starting_no_conditions];
+
+
+if sum(conditions_array(2,:)) > (4*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats)  % this formula ensures that it is mathematical possible to create a list with the restriction of max_repeats of the same consecutive conditions
+    disp('starting proportions do not allow the restricted randomization');
+end
+if sum(conditions_array(2,:)) == 0
+    disp('conditions_array is empty');
+end
+
+if isempty(rnd_condition_order)
+    rnd_condition_order = free_conditions(randperm(length(free_conditions), max_repeats));
+    while length(rnd_condition_order)~=10
+        if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+            danger_for_repeat = rnd_condition_order(end);
+        else
+            danger_for_repeat = [];
+        end
+        not_excluded_conditions = find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+            (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats));
+        if isempty(not_excluded_conditions)
+            not_excluded_conditions = conditions_array(1,:);
+        end
+        not_empty_conditions = find(conditions_array(2, :)~=0);
+        available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+        available_conditions = setdiff(available_conditions, danger_for_repeat);
+        available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+        added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+        rnd_condition_order(end+1) = added_condition;
+    end
+else
+    rnd_condition_order = rnd_condition_order(2:10);
+    if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+        danger_for_repeat = rnd_condition_order(end);
+    else
+        danger_for_repeat = [];
+    end
+    not_excluded_conditions = find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+        (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats));
+    if isempty(not_excluded_conditions)
+        not_excluded_conditions = conditions_array(1,:);
+    end
+    not_empty_conditions = find(conditions_array(2, :)~=0);
+    available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+    available_conditions = setdiff(available_conditions, danger_for_repeat);
+    available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+    added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+    rnd_condition_order(end+1) = added_condition;
+end
+end
