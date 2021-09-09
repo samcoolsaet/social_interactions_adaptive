1ffc9cd9b71ba9092f517f618756e4b2fc91ff66
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-09-09 17:30:40 -0400

    update

--- conditionOrder.m
+++ conditionOrder.m
@@ -1,76 +1,133 @@
-function [conditions_array, rnd_condition_order] = conditionOrder(conditions_array, conditions, max_repeats, rnd_condition_order)
-% [conditions_array, rnd_condition_order] = conditionOrder(conditions_array, conditions, max_repeats, rnd_condition_order)
-if sum(conditions_array(2,:)) > (4*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats)  % this formula ensures that it is mathematical possible to create a list with the restriction of max_repeats of the same consecutive conditions
+function [conditions_array, rnd_condition_order] = conditionOrder(structure, max_repeats, rnd_condition_order,...
+    categorizing, agent_patient)
+
+struct_conditions = zeros(length(structure), 3);
+for i = 1:length(structure)
+    struct_conditions(i, :) = structure(i).condition;
+end
+% ik had array 1:0 2:1 3:3 en hij had toch conditie 1 vooraan in de rij!!!!!!!
+% ik denk dat dit komt omdat we een random conditie order maken gebasseerd op de huidige array en dan maar 1 conditie aftrekken van de array en hierop verder bouwen!
+
+if categorizing
+    active_conditions = struct_conditions(:,1);
+    not_completed_indexes = [structure.c_completed]==0;
+    free_conditions = active_conditions(not_completed_indexes)';
+elseif agent_patient
+    a_active_conditions = struct_conditions(:,2);
+    p_active_conditions = struct_conditions(:,3);
+    active_conditions = [a_active_conditions p_active_conditions];
+    not_completed_indexes = [[structure.a_completed]==0; [structure.p_completed]==0];
+    a_available = a_active_conditions(not_completed_indexes(1,:));
+    p_available = p_active_conditions(not_completed_indexes(2,:));
+    free_conditions = [a_available' p_available'];
+elseif ~agent_patient && ~categorizing    
+    c_active_conditions = struct_conditions(:,1);
+    a_active_conditions = struct_conditions(:,2);
+    p_active_conditions = struct_conditions(:,3);
+    a_active_conditions = [c_active_conditions a_active_conditions p_active_conditions];
+    not_completed_indexes = [[structure.c_completed]==0; [structure.a_completed]==0; [structure.p_completed]==0];
+    c_available = c_active_conditions(not_completed_indexes(1,:));
+    a_available = a_active_conditions(not_completed_indexes(2,:));
+    p_available = p_active_conditions(not_completed_indexes(3,:));
+    free_conditions = [c_available' a_available' p_available'];
+end
+
+starting_no_conditions  = histc(free_conditions, unique(active_conditions));
+
+conditions_array = [unique(active_conditions)'; starting_no_conditions];
+
+maximum_left = max(conditions_array(2,:));
+rest_left = sum(conditions_array(2,:)) - maximum_left;
+% if maximum_left > (rest_left+1)*max_repeats
+%     disp('starting proportions do not allow the restricted randomization');
+% end
+if maximum_left > (rest_left)*max_repeats
     disp('starting proportions do not allow the restricted randomization');
 end
+% if sum(conditions_array(2,:)) > (4*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats)  % this formula ensures that it is mathematical possible to create a list with the restriction of max_repeats of the same consecutive conditions
+%     disp('starting proportions do not allow the restricted randomization');
+% end
 if sum(conditions_array(2,:)) == 0
     disp('conditions_array is empty');
 end
-%%%%% ik ga het eerste deel hiervan herschrijven op de manier zoals het 2e
-%%%%% deel, conditie per condititie en geen random meer maken van 10.
-% nu moet ik nog iets maken als we op het einde aangekomen zijn ook.
-% conditions moeten ook geupdated worden of hij gaat in het negatief
-% for i = 1:46
 
-    repetitions = true;
-    repetition = true;
-    new_conditions_array = conditions_array;
-    rnd_condition_order_test = rnd_condition_order;
-    if isempty(rnd_condition_order)
-        rnd_condition_order_test = ...                      
-                conditions(randperm(length(conditions), 10));
-        while repetitions || repetition
-            repetitions = true;
-            repetition = true;
-            new_conditions_array = conditions_array;
-            unique_condition_order = unique(rnd_condition_order_test);   
-            count_conditions  = histc(rnd_condition_order_test, unique_condition_order);
-            for i = 1:length(unique_condition_order)
-                new_conditions_array(2, find(new_conditions_array(1,:)==unique_condition_order(i))) = ...
-                    new_conditions_array(2, find(new_conditions_array(1,:)==unique_condition_order(i))) - count_conditions(i);
-            end
-            for i = (max_repeats+1):length(rnd_condition_order_test)
-                if sum(rnd_condition_order_test(i-max_repeats:i)==rnd_condition_order_test(i)) == (max_repeats+1)
-                    rnd_condition_order_test = conditions(randperm(length(conditions), 10));
-                else
-                    repetitions = false;
-                end
-            end
-            if max(new_conditions_array(2,:)) > (max_repeats*(sum(new_conditions_array(2,:))-max(new_conditions_array(2,:)))+max_repeats) || ...
-                    any(new_conditions_array(2,:)<0)
-                rnd_condition_order_test = conditions(randperm(length(conditions), 10));
-            else
-                repetition = false;
-            end
-        end
+if isempty(rnd_condition_order)
+        rnd_condition_order = free_conditions(randperm(length(free_conditions), 1));
+%     rnd_condition_order = free_conditions(randperm(length(free_conditions), max_repeats));
+%     while length(rnd_condition_order)~=10
+%         if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+%             danger_for_repeat = rnd_condition_order(end);
+%         else
+%             danger_for_repeat = [];
+%         end
+%         not_excluded_conditions = find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+%             (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats));
+%         if isempty(not_excluded_conditions)
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+%         not_empty_conditions = find(conditions_array(2, :)~=0);
+%         available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+%         available_conditions = setdiff(available_conditions, danger_for_repeat);
+%         available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+%         added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+%         rnd_condition_order(end+1) = added_condition;
+%     end
+else
+    if length(rnd_condition_order) < max_repeats
+        rnd_condition_order(end+1) = free_conditions(randperm(length(free_conditions), 1));
     else
-        rnd_condition_order_test = rnd_condition_order(2:10);
         if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
             danger_for_repeat = rnd_condition_order(end);
         else
             danger_for_repeat = [];
         end
-        not_excluded_conditions = find(new_conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
-            (max_repeats*(sum(new_conditions_array(2,:))-max(new_conditions_array(2,:)))+max_repeats));
-        if isempty(not_excluded_conditions)
-            not_excluded_conditions = new_conditions_array(1,:);
+%         if maximum_left == (rest_left+1)*max_repeats
+%             not_excluded_conditions = conditions_array(1,find(conditions_array(2,:)==maximum_left));
+%         else
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+%         if maximum_left == (rest_left)*max_repeats
+%             not_excluded_conditions = conditions_array(1,find(conditions_array(2,:)==maximum_left));
+%         else
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+%         not_excluded_conditions = conditions_array(1,find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+%             (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats)));
+%         if isempty(not_excluded_conditions)
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+        not_excluded_conditions = conditions_array(1,:);
+        for i = flip(1:length(conditions_array(2,:)))
+            rest = conditions_array(2,:);
+            rest(i) = [];
+            sum_rest = sum(rest);
+            if sum_rest > conditions_array(2,i)*max_repeats
+                not_excluded_conditions(i) = [];
+            end
         end
-        not_empty_conditions = find(new_conditions_array(2, :)~=0);
+        not_empty_conditions = conditions_array(1,find(conditions_array(2, :)~=0));
         available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
         available_conditions = setdiff(available_conditions, danger_for_repeat);
-        available_conditions_list = [];
-        for i = 1:length(available_conditions)
-            available_conditions_list = [available_conditions_list ...
-                new_conditions_array(1, available_conditions(i))*ones(1, new_conditions_array(2, available_conditions(i)))];
-        end
-%         added_condition_index = randperm(length(available_conditions), 1);
-%         added_condition = available_conditions(added_condition_index);
+        available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
         added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
-        rnd_condition_order_test(end+1) = added_condition;
-        new_conditions_array(2, find(new_conditions_array(1,:)==added_condition)) = ...
-            new_conditions_array(2, find(new_conditions_array(1,:)==rnd_condition_order_test(end))) - 1;
+        rnd_condition_order(end+1) = added_condition;
     end
-rnd_condition_order = rnd_condition_order_test;
-conditions_array = new_conditions_array;
-% disp([rnd_condition_order(end)  conditions_array(1, find(new_conditions_array(1,:)==rnd_condition_order(end)))]);
-% end
+%     rnd_condition_order = rnd_condition_order(2:10);
+%     if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+%         danger_for_repeat = rnd_condition_order(end);
+%     else
+%         danger_for_repeat = [];
+%     end
+%     not_excluded_conditions = find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+%         (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats));
+%     if isempty(not_excluded_conditions)
+%         not_excluded_conditions = conditions_array(1,:);
+%     end
+%     not_empty_conditions = find(conditions_array(2, :)~=0);
+%     available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+%     available_conditions = setdiff(available_conditions, danger_for_repeat);
+%     available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+%     added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+%     rnd_condition_order(end+1) = added_condition;
+end
+end
--- old scripts/createStructure.m
+++ old scripts/createStructure.m
@@ -1,25 +1,21 @@
-function [structure] = Copy_of_createStructure(stimulus_list, frame_list, cum_length, chasing_folder,...
+function [structure] = createStructure(stimulus_list, frame_list, cum_length, chasing_folder,...
     gen_chasing_folder, grooming_folder, gen_grooming_folder, mounting_folder, gen_mounting_folder,...
     holding_folder, gen_holding_folder)
 structure = struct('stimuli', {}, 'frames', {}, 'c_fails', {}, ... 
-    'c_success', {},'c_completed', {}, 'c_last_block', {}, 'a_fails', {}, 'a_success', {}, ...
-    'a_completed', {}, 'a_last_block', {}, 'p_fails', {}, 'p_success', {}, 'p_completed', {},...
-    'p_last_block', {}, 'folder', {}, 'condition', {});
+    'c_success', {},'c_completed', {}, 'a_fails', {}, 'a_success', {}, ...
+    'a_completed', {}, 'p_fails', {}, 'p_success', {}, 'p_completed', {}, 'folder', {}, 'condition', {});
 for i = 1:length(stimulus_list)
     structure(i).stimuli = stimulus_list(i);
     structure(i).frames = frame_list(i);
     structure(i).c_fails = 0;
     structure(i).c_success = 0;
     structure(i).c_completed = 0;
-    structure(i).c_last_block = 0;
     structure(i).a_fails = 0;
     structure(i).a_success = 0;
     structure(i).a_completed = 0;
-    structure(i).a_last_block = 0;
     structure(i).p_fails = 0;
     structure(i).p_success = 0;
     structure(i).p_completed = 0;
-    structure(i).p_last_block = 0;
     if i <= cum_length(1)
         structure(i).folder = chasing_folder;
         structure(i).condition = [1 5 6];
