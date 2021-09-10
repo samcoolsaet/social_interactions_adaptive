0eddcfe43d3ba38639c84fe9d61fbd4d1e4450f6
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-09-10 12:50:46 -0400

    update

---
+++ Copy_of_conditionOrder.m
@@ -0,0 +1,154 @@
+function [conditions_array, rnd_condition_order] = conditionOrder(structure, max_repeats, rnd_condition_order,...
+    categorizing, agent_patient)
+
+struct_conditions = cell(1);
+for i = 1:length(structure)
+    struct_conditions(i, 1:length(structure(i).condition)) = num2cell(structure(i).condition);
+end
+% parallel in 3e dimense booleans creeren voor de struct_conditions om de
+% niet gebruikt te isoleren
+for i = 1:length(struct_conditions(:,1))
+    for j = 1:length(struct_conditions(1,:))
+        if ~isempty(struct_conditions(i,j))
+            if ismember(struct_conditions{i,j}, [1 2 3 4])
+                struct_conditions{i,j,2} = structure(i).c_completed;
+            end
+            if struct_conditions{i,j} == 5
+                struct_conditions{i,j,2} = structure(i).a_completed;
+            end
+            if struct_conditions{i,j} == 6 
+                struct_conditions{i,j,2} = structure(i).p_completed;
+            end
+            if struct_conditions{i,j} == 7
+                struct_conditions{i,j,2} = structure(i).b_completed;
+            end
+        else
+            struct_conditions{i,j,2} = 99;
+        end
+    end
+end
+num_array_struct_conditions = cell2mat(struct_conditions);    
+
+if categorizing
+    active_conditions = struct_conditions(:,1);
+    not_completed_indexes = [structure.c_completed]==0;
+    free_conditions = active_conditions(not_completed_indexes)';
+elseif agent_patient
+    a_active_conditions = struct_conditions(:,2);
+    p_active_conditions = struct_conditions(:,3);
+    active_conditions = [a_active_conditions p_active_conditions];
+    not_completed_indexes = [[structure.a_completed]==0; [structure.p_completed]==0];
+    a_available = a_active_conditions(not_completed_indexes(1,:));
+    p_available = p_active_conditions(not_completed_indexes(2,:));
+    free_conditions = [a_available' p_available'];
+elseif ~agent_patient && ~categorizing    
+    c_active_conditions = struct_conditions(:,1);
+    a_active_conditions = struct_conditions(:,2);
+    p_active_conditions = struct_conditions(:,3);
+    a_active_conditions = [c_active_conditions a_active_conditions p_active_conditions];
+    not_completed_indexes = [[structure.c_completed]==0; [structure.a_completed]==0; [structure.p_completed]==0];
+    c_available = c_active_conditions(not_completed_indexes(1,:));
+    a_available = a_active_conditions(not_completed_indexes(2,:));
+    p_available = p_active_conditions(not_completed_indexes(3,:));
+    free_conditions = [c_available' a_available' p_available'];
+end
+
+starting_no_conditions  = histc(free_conditions, unique(active_conditions));
+
+conditions_array = [unique(active_conditions)'; starting_no_conditions];
+
+maximum_left = max(conditions_array(2,:));
+rest_left = sum(conditions_array(2,:)) - maximum_left;
+% if maximum_left > (rest_left+1)*max_repeats
+%     disp('starting proportions do not allow the restricted randomization');
+% end
+if maximum_left > (rest_left)*max_repeats
+    disp('starting proportions do not allow the restricted randomization');
+end
+% if sum(conditions_array(2,:)) > (4*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats)  % this formula ensures that it is mathematical possible to create a list with the restriction of max_repeats of the same consecutive conditions
+%     disp('starting proportions do not allow the restricted randomization');
+% end
+if sum(conditions_array(2,:)) == 0
+    disp('conditions_array is empty');
+end
+
+if isempty(rnd_condition_order)
+        rnd_condition_order = free_conditions(randperm(length(free_conditions), 1));
+%     rnd_condition_order = free_conditions(randperm(length(free_conditions), max_repeats));
+%     while length(rnd_condition_order)~=10
+%         if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+%             danger_for_repeat = rnd_condition_order(end);
+%         else
+%             danger_for_repeat = [];
+%         end
+%         not_excluded_conditions = find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+%             (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats));
+%         if isempty(not_excluded_conditions)
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+%         not_empty_conditions = find(conditions_array(2, :)~=0);
+%         available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+%         available_conditions = setdiff(available_conditions, danger_for_repeat);
+%         available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+%         added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+%         rnd_condition_order(end+1) = added_condition;
+%     end
+else
+    if length(rnd_condition_order) < max_repeats
+        rnd_condition_order(end+1) = free_conditions(randperm(length(free_conditions), 1));
+    else
+        if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+            danger_for_repeat = rnd_condition_order(end);
+        else
+            danger_for_repeat = [];
+        end
+%         if maximum_left == (rest_left+1)*max_repeats
+%             not_excluded_conditions = conditions_array(1,find(conditions_array(2,:)==maximum_left));
+%         else
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+%         if maximum_left == (rest_left)*max_repeats
+%             not_excluded_conditions = conditions_array(1,find(conditions_array(2,:)==maximum_left));
+%         else
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+%         not_excluded_conditions = conditions_array(1,find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+%             (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats)));
+%         if isempty(not_excluded_conditions)
+%             not_excluded_conditions = conditions_array(1,:);
+%         end
+        not_excluded_conditions = conditions_array(1,:);
+        for i = flip(1:length(conditions_array(2,:)))
+            rest = conditions_array(2,:);
+            rest(i) = [];
+            sum_rest = sum(rest);
+            if sum_rest > conditions_array(2,i)*max_repeats
+                not_excluded_conditions(i) = [];
+            end
+        end
+        not_empty_conditions = conditions_array(1,find(conditions_array(2, :)~=0));
+        available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+        available_conditions = setdiff(available_conditions, danger_for_repeat);
+        available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+        added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+        rnd_condition_order(end+1) = added_condition;
+    end
+%     rnd_condition_order = rnd_condition_order(2:10);
+%     if sum(rnd_condition_order(end-(max_repeats-1):end)==rnd_condition_order(end)) == (max_repeats)
+%         danger_for_repeat = rnd_condition_order(end);
+%     else
+%         danger_for_repeat = [];
+%     end
+%     not_excluded_conditions = find(conditions_array(2,:) == ...     % this is based on having enough of a condition left to fullfil the no more than 3 repeats criterium
+%         (max_repeats*(sum(conditions_array(2,:))-max(conditions_array(2,:)))+max_repeats));
+%     if isempty(not_excluded_conditions)
+%         not_excluded_conditions = conditions_array(1,:);
+%     end
+%     not_empty_conditions = find(conditions_array(2, :)~=0);
+%     available_conditions = intersect(not_excluded_conditions, not_empty_conditions);
+%     available_conditions = setdiff(available_conditions, danger_for_repeat);
+%     available_conditions_list = free_conditions(ismember(free_conditions, available_conditions));
+%     added_condition = available_conditions_list(randperm(length(available_conditions_list), 1));
+%     rnd_condition_order(end+1) = added_condition;
+end
+end
--- Copy_of_createStructure.m
+++ Copy_of_createStructure.m
@@ -1,16 +1,17 @@
-function [structure] = createStructure(stimulus_list, frame_list, cum_length, chasing_folder,...
+function [structure] = Copy_of_createStructure(stimulus_list, frame_list, cum_length, chasing_folder,...
     gen_chasing_folder, grooming_folder, gen_grooming_folder, mounting_folder, gen_mounting_folder,...
-    holding_folder, gen_holding_folder)
+    holding_folder, gen_holding_folder, categorizing, agent_patient)
 structure = struct('stimuli', {}, 'frames', {}, 'c_fails', {}, ... 
     'c_success', {},'c_completed', {}, 'c_last_block', {}, 'a_fails', {}, 'a_success', {}, ...
     'a_completed', {}, 'a_last_block', {}, 'p_fails', {}, 'p_success', {}, 'p_completed', {},...
     'p_last_block', {}, 'b_fails', {}, 'b_success', {}, ...
     'b_completed', {}, 'b_last_block', {}, 'folder', {}, 'condition', {});
-load('inventory.mat');
+if agent_patient
+    load('inventory.mat', 'inventory');
+end
 for i = 1:length(stimulus_list)
     structure(i).stimuli = stimulus_list(i);
     structure(i).frames = frame_list(i);
-    
     structure(i).c_fails = 0;
     structure(i).c_success = 0;
     structure(i).c_completed = 0;
@@ -27,22 +28,85 @@ for i = 1:length(stimulus_list)
     structure(i).b_success = 0;
     structure(i).b_completed = 0;
     structure(i).b_last_block = 0;
-    if i <= cum_length(1)
-        structure(i).folder = chasing_folder;
-    elseif i <= cum_length(2)
-        structure(i).folder = gen_chasing_folder;
-    elseif i <= cum_length(3)
-        structure(i).folder = grooming_folder;
-    elseif i <= cum_length(4)
-        structure(i).folder = gen_grooming_folder;
-    elseif i <= cum_length(5)
-        structure(i).folder = mounting_folder;
-    elseif i <= cum_length(6)
-        structure(i).folder = gen_mounting_folder;
-    elseif i <= cum_length(7)
-        structure(i).folder = holding_folder;
-    elseif i <= cum_length(8)
-        structure(i).folder = gen_holding_folder;
+    if categorizing && agent_patient       
+        inv_index = contains([inventory.name], stimulus_list{i}(1:end-3));
+        conditions = ~[isempty(inventory(inv_index).a_degrees) ...
+            isempty(inventory(inv_index).p_degrees) ...
+            isempty(inventory(inv_index).b_degrees)] .* [5 6 7];
+        if i <= cum_length(1)
+            structure(i).folder = chasing_folder;
+            structure(i).condition = [1 conditions(conditions~=0)];
+        elseif i <= cum_length(2)
+            structure(i).folder = gen_chasing_folder;
+            structure(i).condition = [1 conditions(conditions~=0)];
+        elseif i <= cum_length(3)
+            structure(i).folder = grooming_folder;
+            structure(i).condition = [2 conditions(conditions~=0)];
+        elseif i <= cum_length(4)
+            structure(i).folder = gen_grooming_folder;
+            structure(i).condition = [2 conditions(conditions~=0)];
+        elseif i <= cum_length(5)
+            structure(i).folder = mounting_folder;
+            structure(i).condition = [3 conditions(conditions~=0)];
+        elseif i <= cum_length(6)
+            structure(i).folder = gen_mounting_folder;
+            structure(i).condition = [3 conditions(conditions~=0)];
+        elseif i <= cum_length(7)
+            structure(i).folder = holding_folder;
+            structure(i).condition = [4 conditions(conditions~=0)];
+        elseif i <= cum_length(8)
+            structure(i).folder = gen_holding_folder;
+            structure(i).condition = [4 conditions(conditions~=0)];
+        end
+    elseif agent_patient
+        inv_index = contains([inventory.name], stimulus_list{i}(1:end-3));
+        conditions = ~[isempty(inventory(inv_index).a_degrees) ...
+            isempty(inventory(inv_index).p_degrees) ...
+            isempty(inventory(inv_index).b_degrees)] .* [5 6 7];
+        structure(i).condition = conditions(conditions~=0);
+        if i <= cum_length(1)
+            structure(i).folder = chasing_folder;
+        elseif i <= cum_length(2)
+            structure(i).folder = gen_chasing_folder;
+        elseif i <= cum_length(3)
+            structure(i).folder = grooming_folder;
+        elseif i <= cum_length(4)
+            structure(i).folder = gen_grooming_folder;
+        elseif i <= cum_length(5)
+            structure(i).folder = mounting_folder;
+        elseif i <= cum_length(6)
+            structure(i).folder = gen_mounting_folder;
+        elseif i <= cum_length(7)
+            structure(i).folder = holding_folder;
+        elseif i <= cum_length(8)
+            structure(i).folder = gen_holding_folder;
+        end
+    elseif categorizing
+        if i <= cum_length(1)
+            structure(i).folder = chasing_folder;
+            structure(i).condition = 1;
+        elseif i <= cum_length(2)
+            structure(i).folder = gen_chasing_folder;
+            structure(i).condition = 1;
+        elseif i <= cum_length(3)
+            structure(i).folder = grooming_folder;
+            structure(i).condition = 2;
+        elseif i <= cum_length(4)
+            structure(i).folder = gen_grooming_folder;
+            structure(i).condition = 2;
+        elseif i <= cum_length(5)
+            structure(i).folder = mounting_folder;
+            structure(i).condition = 3;
+        elseif i <= cum_length(6)
+            structure(i).folder = gen_mounting_folder;
+            structure(i).condition = 3;
+        elseif i <= cum_length(7)
+            structure(i).folder = holding_folder;
+            structure(i).condition = 4;
+        elseif i <= cum_length(8)
+            structure(i).folder = gen_holding_folder;
+            structure(i).condition = 4;
+        end
     end
 end
 disp('new structure made');
