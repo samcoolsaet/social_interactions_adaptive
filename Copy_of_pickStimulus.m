771f6e74024f4c9200527f99e7b0b0f292e5caab
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-09-13 16:54:08 -0400

    update

--- Copy_of_pickStimulus.m
+++ Copy_of_pickStimulus.m
@@ -1,88 +1,48 @@
 function [struct_index] = Copy_of_pickStimulus(condition,structure, struct_conditions)
 
-struct_conditions = zeros(length(structure), 3);
-for i = 1:length(structure)
-    struct_conditions(i, :) = structure(i).condition;
-end
 switch condition
     case 1
-        not_completed = find([structure.c_completed]==0);
-        [this_condition placeholder] = find(struct_conditions==1);
-        available_indexes = intersect(not_completed, this_condition);
+        this_condition_indexes = find(cellfun(@(x) x==1, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
         struct_index = available_indexes(randperm(length(available_indexes), 1));
-%         index3 = 1;
-%         indexes_c_incomplete = [];
-%         while index3 ~= length(structure) + 1          % just iterate over the structure and see which ones are still available (not completed), given the condition
-%             if (strcmp(structure(index3).folder, 'chasing') || ...
-%                     strcmp(structure(index3).folder, 'gen_chasing'))...
-%                     && structure(index3).c_completed == 0
-%                 indexes_c_incomplete(end+1) = index3;
-%             end
-%             index3 = index3 + 1;
-%         end
-%         index_index = randperm(length(indexes_c_incomplete), 1);
-%         struct_index = indexes_c_incomplete(index_index);
     case 2
-        not_completed = find([structure.c_completed]==0);
-        [this_condition placeholder] = find(struct_conditions==2);
-        available_indexes = intersect(not_completed, this_condition);
+        this_condition_indexes = find(cellfun(@(x) x==2, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
         struct_index = available_indexes(randperm(length(available_indexes), 1));
-%         index3 = 1;
-%         indexes_c_incomplete = [];
-%         while index3 ~= length(structure) + 1
-%             if (strcmp(structure(index3).folder, 'grooming') || ...
-%                     strcmp(structure(index3).folder, 'gen_grooming'))...
-%                     && structure(index3).c_completed == 0
-%                 indexes_c_incomplete(end+1) = index3;
-%             end
-%             index3 = index3 + 1;
-%         end
-%         index_index = randperm(length(indexes_c_incomplete), 1);
-%         struct_index = indexes_c_incomplete(index_index);
     case 3
-        not_completed = find([structure.c_completed]==0);
-        [this_condition placeholder] = find(struct_conditions==3);
-        available_indexes = intersect(not_completed, this_condition);
+        this_condition_indexes = find(cellfun(@(x) x==3, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
         struct_index = available_indexes(randperm(length(available_indexes), 1));
-%         index3 = 1;
-%         indexes_c_incomplete = [];
-%         while index3 ~= length(structure) + 1
-%             if (strcmp(structure(index3).folder, 'mounting') || ...
-%                     strcmp(structure(index3).folder, 'gen_mounting'))...
-%                     && structure(index3).c_completed == 0
-%                 indexes_c_incomplete(end+1) = index3;
-%             end
-%             index3 = index3 + 1;
-%         end
-%         index_index = randperm(length(indexes_c_incomplete), 1);
-%         struct_index = indexes_c_incomplete(index_index);
     case 4
-        not_completed = find([structure.c_completed]==0);
-        [this_condition placeholder] = find(struct_conditions==4);
-        available_indexes = intersect(not_completed, this_condition);
+        this_condition_indexes = find(cellfun(@(x) x==4, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
         struct_index = available_indexes(randperm(length(available_indexes), 1));
-%         index3 = 1;
-%         indexes_c_incomplete = [];
-%         while index3 ~= length(structure) + 1
-%             if (strcmp(structure(index3).folder, 'holding') || ...
-%                     strcmp(structure(index3).folder, 'gen_holding'))...
-%                     && structure(index3).c_completed == 0
-%                 indexes_c_incomplete(end+1) = index3;
-%             end
-%             index3 = index3 + 1;
-%         end
-%         index_index = randperm(length(indexes_c_incomplete), 1);
-%         struct_index = indexes_c_incomplete(index_index);
     case 5
-        available_indexes = find([structure.a_completed]==0);
+        this_condition_indexes = find(cellfun(@(x) x==5, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
         struct_index = available_indexes(randperm(length(available_indexes), 1));
-%         index_index = randperm(length(indexes_a_incomplete), 1);
-%         struct_index = indexes_a_incomplete(index_index);
     case 6
-        available_indexes = find([structure.p_completed]==0);
+        this_condition_indexes = find(cellfun(@(x) x==6, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
         struct_index = available_indexes(randperm(length(available_indexes), 1));
-%         index_index = randperm(length(indexes_p_incomplete), 1);
-%         struct_index = indexes_p_incomplete(index_index);
+    case 7 % cellfun(@(x) x==1,a)
+        this_condition_indexes = find(cellfun(@(x) x==7, struct_conditions(:,:,1)));
+        [not_completed_indexes, ~] = ind2sub(size(struct_conditions(:,:,1)),...
+            find(cellfun(@(x) x==0, struct_conditions(:,:,2))));
+        available_indexes = intersect(this_condition_indexes, not_completed_indexes(1:length(structure)));
+        struct_index = available_indexes(randperm(length(available_indexes), 1));
     otherwise
         disp('indexing into structure failed');
 end
