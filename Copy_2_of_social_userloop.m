d2e75de82bae975fc400d152cea8c184f16d8c65
samcoolsaet <80270714+samcoolsaet@users.noreply.github.com>
2021-09-27 10:46:04 -0400

    update

--- Copy_2_of_social_userloop.m
+++ Copy_2_of_social_userloop.m
@@ -11,18 +11,18 @@ persistent timing_filenames_retrieved
     return
     end
 %% initializing for first trial
-TrialRecord.User.start_block = 21;                                               % the progression number to start training with
+TrialRecord.User.start_block = 1;                                               % the progression number to start training with
 TrialRecord.User.generalizing = true;
 
 if TrialRecord.CurrentTrialNumber == 0
-%     TrialRecord.User.num_cats = 1;
+    TrialRecord.User.num_cats = 1;
     TrialRecord.User.next_stim_index = 1;
     TrialRecord.User.next_stim = false;
+    TrialRecord.User.num_cats_changed = false;
     TrialRecord.User.repeat_vector = [];
     TrialRecord.User.performance = 0;
     TrialRecord.NextBlock = TrialRecord.User.start_block;
     previous_sum_buttons = 0;
-    previous_sum_categories = 0;
     TrialRecord.User.engaged = true;
     TrialRecord.User.max_fails = 6;
     TrialRecord.User.repeat = false;
@@ -34,7 +34,6 @@ if TrialRecord.CurrentTrialNumber == 0
     TrialRecord.User.structure_completion = 0;
 else
     previous_sum_buttons = TrialRecord.User.current_sum_buttons;      % calculations of previous sum categories
-    previous_sum_categories = TrialRecord.User.current_sum_categories;    
 end
 %% constants
 % progression
@@ -46,10 +45,6 @@ fail_threshold = 0.10;                                                         %
 
 TrialRecord.User.size_progression_factor = 20;                              % the number of progression number steps needed to go from start size to end size, used for both category and agent patient
 
-TrialRecord.User.button_progression_factor = ...
-    TrialRecord.User.size_progression_factor + 1;                            % number of progression number steps needed to add a category button
-                                                                            % progression_trials 
-                                                                            
 TrialRecord.User.max_block = 20; % TrialRecord.User.button_progression_factor * 3 ...
 %    - 1;                                                                    % last button active + at final size
 TrialRecord.User.min_block = TrialRecord.User.start_block;
@@ -79,16 +74,6 @@ TrialRecord.User.mounting = false;
 
 
 %% determining next block and difficulty based on a general progression number %%%%%% maybe create a vector with the length of trialerrors but displaying the stimulus sequence numbers, this way I can keep track of actual fails and just going on when failing because the number of repeats hits the limit
-% if TrialRecord.CurrentTrialNumber ~= 0
-%     cum_last_block = sum([TrialRecord.User.structure.c_last_block]) + ...
-%         sum([TrialRecord.User.structure.a_last_block]) + ...
-%         sum([TrialRecord.User.structure.p_last_block]);
-%     if cum_last_block == TrialRecord.User.blocksize 
-%         [TrialRecord.User.structure, TrialRecord.User.performance, TrialRecord.NextBlock] = ...
-%             evaluate(TrialRecord.User.structure, TrialRecord.CurrentBlock, succes_threshold, fail_threshold);
-%     end
-% end
-
 if sum(TrialRecord.User.repeat_vector == 0) == TrialRecord.User.blocksize
     [TrialRecord.User.performance, TrialRecord.User.NextBlock, TrialRecord.User.repeat_vector] = ...
         Copy_of_evaluate(TrialRecord.TrialErrors, TrialRecord.User.repeat_vector, succes_threshold, fail_threshold, TrialRecord.CurrentBlock);
@@ -107,48 +92,34 @@ elseif TrialRecord.NextBlock < TrialRecord.User.min_block
 end
 % setting independant category and button progression based on progression
 % number
-TrialRecord.User.button_progression = ...                                 % the category progression factor, which should be at least bigger than the size progression factor in order 
-    TrialRecord.NextBlock / ...                               % in order to go through the complete size evolution before adding a category, determines the number of progression
-    (TrialRecord.User.button_progression_factor);                                          % number steps needed to increase category progression + 1
-
 TrialRecord.User.size_progression = ... 
-    mod(TrialRecord.NextBlock, ... 
-    TrialRecord.User.button_progression_factor);
+    TrialRecord.NextBlock / TrialRecord.User.size_progression_factor;
 
 %% toggling conditions on
 % training progression switches. at the end ( if not training ) all switches
 % are turned on 
-if TrialRecord.User.training_categorization
-    TrialRecord.User.category = true;
-    if TrialRecord.User.button_progression >=0
-        TrialRecord.User.chasing_on = true;
-    end
-    if TrialRecord.User.button_progression >=1
-        TrialRecord.User.grooming_on = true;
-    end
-    if TrialRecord.User.button_progression >=2
-        TrialRecord.User.mounting_on = true;
-    end
-    if TrialRecord.User.button_progression >=3
-        TrialRecord.User.holding_on = true;
-    end
-elseif TrialRecord.User.training_agent_patient
+if TrialRecord.User.num_cats >=1
+    TrialRecord.User.chasing_on = true;
+end
+if TrialRecord.User.num_cats >=2
+    TrialRecord.User.grooming_on = true;
+end
+if TrialRecord.User.num_cats >=3
+    TrialRecord.User.mounting_on = true;
+end
+if TrialRecord.User.num_cats >=4
+    TrialRecord.User.holding_on = true;
+end
+if TrialRecord.User.training_agent_patient
     TrialRecord.User.agent_on = true;
     TrialRecord.User.patient_on = true;
     TrialRecord.User.bystander = true;
     TrialRecord.User.category = false;
-    if TrialRecord.User.button_progression >=0
-        TrialRecord.User.chasing_on = true;
-    end
-    if TrialRecord.User.button_progression >=1
-        TrialRecord.User.grooming_on = true;
-    end
-    if TrialRecord.User.button_progression >=2
-        TrialRecord.User.mounting_on = true;
-    end
-    if TrialRecord.User.button_progression >=3
-        TrialRecord.User.holding_on = true;
-    end
+    TrialRecord.User.current_sum_buttons = sum([TrialRecord.User.agent_on, ...  
+    TrialRecord.User.patient_on, TrialRecord.User.bystander]);
+elseif TrialRecord.User.training_categorization
+    TrialRecord.User.category = true;
+    TrialRecord.User.current_sum_buttons = TrialRecord.User.num_cats;
 else
     TrialRecord.User.chasing_on = true;
     TrialRecord.User.grooming_on = true;
@@ -156,29 +127,14 @@ else
     TrialRecord.User.mounting_on = true;
     TrialRecord.User.agent_on = true;
     TrialRecord.User.patient_on = true;
+    TrialRecord.User.bystander = true;
     TrialRecord.User.category = true;
-end
-
-% calculation current sum categories after switches have been altered
-
-TrialRecord.User.current_sum_categories = sum([TrialRecord.User.chasing_on, ... % difference between current and previous sum of categories acts as a 
-    TrialRecord.User.grooming_on,TrialRecord.User.holding_on, ...               % switch for new stimulus list creation involving new categories and other
-    TrialRecord.User.mounting_on,]);
-display(TrialRecord.User.current_sum_categories);
-
-if TrialRecord.User.training_categorization
-    TrialRecord.User.current_sum_buttons = sum([TrialRecord.User.chasing_on, ... 
-        TrialRecord.User.grooming_on,TrialRecord.User.holding_on, ...           
-        TrialRecord.User.mounting_on]);
-elseif TrialRecord.User.training_agent_patient   
-    TrialRecord.User.current_sum_buttons = sum([TrialRecord.User.agent_on, ...  
-        TrialRecord.User.patient_on, TrialRecord.User.bystander]);
-else   
     TrialRecord.User.current_sum_buttons = sum([TrialRecord.User.chasing_on, ... % difference between current and previous sum of categories acts as a 
-        TrialRecord.User.grooming_on,TrialRecord.User.holding_on, ...               % switch for new stimulus list creation involving new categories and other
-        TrialRecord.User.mounting_on, TrialRecord.User.agent_on, ...                % changes that have to be made when categories are added
-        TrialRecord.User.patient_on]);
+    TrialRecord.User.grooming_on,TrialRecord.User.holding_on, ...               % switch for new stimulus list creation involving new categories and other
+    TrialRecord.User.mounting_on, TrialRecord.User.agent_on, ...                % changes that have to be made when categories are added
+    TrialRecord.User.patient_on, TrialRecord.User.bystander]);
 end
+
 %% orienting between stimuli and frame files creating individual folder lists and general stim and frame lists
 % Do all of this for trial 0 because it would be inefficient to repeat and
 % I want these variables to be fixed throughout the run
@@ -220,9 +176,10 @@ end
 
 stimulus_list = stimulus_list(mod(TrialRecord.User.next_stim_index, length(frame_list))+1);
 %% create an empty structure
-if TrialRecord.User.current_sum_categories ~= previous_sum_categories || ...
+if TrialRecord.User.num_cats_changed || ...
         TrialRecord.User.next_stim                                          % this comes dozn to: on start and zhen button added within training
     TrialRecord.User.next_stim = false;
+    TrialRecord.User.num_cats_changed
     [TrialRecord.User.structure] = Copy_of_createStructure(stimulus_list, frame_list, cum_length, TrialRecord.User.chasing_folder,...
     TrialRecord.User.gen_chasing_folder, TrialRecord.User.grooming_folder, TrialRecord.User.gen_grooming_folder, ...
     TrialRecord.User.mounting_folder, TrialRecord.User.gen_mounting_folder,...
